54b46104a2eab131305c5914116d99f53353ae1f
Kristoffer Grönlund <krig@koru.se>
2014-02-09 02:29:49 +0100

    flappy bird clone :p

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,67 @@
 #include "common.hpp"
 #include "rand.hpp"
+#include <math.h>
 
 namespace {
+	const float GRAVITY = 9.81f * 1.2f;//1.33f;
+	const float DT = 1.f / 60.f;
+	const float PI = 3.14159265358979323846f;
+	const float SCR_W = 270.f;
+	const float SCR_H = 480.f;
+
 	struct game_data {
-		SDL_Window* window;
-		SDL_Renderer* rnd;
-		bool running;
-
-		SDL_Texture* screen;
-		Uint32* pixels;
-
 		game_data() {
-			window = nullptr;
-			rnd = nullptr;
 			running = true;
-			pixels = new Uint32[320 * 200];
 		}
 
 		~game_data() {
-			delete [] pixels;
+		}
+
+		sdl::screen screen;
+		sdl::sprite bg;
+		sdl::sprite hills;
+		sdl::sprite cloud;
+		sdl::numbers npass;
+		bool running;
+	};
+
+	struct player_data {
+		float x;
+		float y;
+		float velocity;
+
+		sdl::animated_sprite img;
+
+		player_data() {
+			x = SCR_W*0.5f;
+			y = SCR_H*0.5f;
+			velocity = 0;
 		}
 	};
 
-	struct sprite {
+	struct pipes_data {
 		
 	};
 
 	game_data game;
+	player_data player;
+	pipes_data pipes;
+
+	int num_pass = 0;
+	float hills_pos = 0;
+	float cloud_pos = 0;
 
 	void init() {
-		game.window = SDL_CreateWindow("simplex",
-		                               SDL_WINDOWPOS_UNDEFINED,
-		                               SDL_WINDOWPOS_UNDEFINED,
-		                               960,
-		                               600,
-		                               0);
-		if (game.window == nullptr) {
-			throw sdl::error("%s", SDL_GetError());
-		}
-		// | SDL_RENDERER_PRESENTVSYNC
-		game.rnd = SDL_CreateRenderer(game.window, -1,
-		                              SDL_RENDERER_ACCELERATED);
-		if (game.rnd == nullptr){
-			throw sdl::error("%s", SDL_GetError());
-		}
-		SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
-		SDL_RenderSetLogicalSize(game.rnd, 320, 200);
-
-		game.screen = SDL_CreateTexture(game.rnd,
-		                                SDL_PIXELFORMAT_ARGB8888,
-		                                SDL_TEXTUREACCESS_STREAMING,
-		                                320, 200);
-
-		for (int y = 0; y < 200; ++y) {
-			for (int x = 0; x < 320; ++x) {
-				game.pixels[(199 - y)*320 + x] = 0xff101010 + (23 + y);
-			}
-		}
-		for (int i = 0; i < 64; ++i) {
-			uint64_t rnd = util::randint();
-			int x = (rnd >> 32) % 320;
-			int y = rnd % 200;
-			game.pixels[y*320 + x] = 0xffffffff;
-		}
+		game.screen.create((int)SCR_W, (int)SCR_H, 540, 960);
+		game.bg.load(game.screen, "data/bg0.png");
+		game.hills.load(game.screen, "data/bg1.png");
+		game.cloud.load(game.screen, "data/cloud.png");
+		game.npass.load(game.screen, "data/numbers.png");
+		player.img.load(game.screen, "data/bird.png");
+		player.img.init_anim(3, 4);
 	}
+
+	void player_jump();
 
 	void handle_event(SDL_Event* e) {
 		if (e->type == SDL_QUIT) {
@@ -73,16 +71,81 @@ namespace {
 			if (e->key.keysym.sym == SDLK_ESCAPE) {
 				game.running = false;
 			}
+			else if (e->key.keysym.sym == SDLK_SPACE) {
+				player_jump();
+			}
 		}
 	}
 
+	void update_background() {
+		hills_pos -= 32.0f * DT;
+		if (hills_pos <= -SCR_W)
+			hills_pos += SCR_W;
+		cloud_pos -= 10.0f * DT;
+		if (cloud_pos <= -SCR_W)
+			cloud_pos += SCR_W;
+	}
+
+	void update_player() {
+		player.velocity -= GRAVITY * DT;
+		if (player.velocity < -GRAVITY)
+			player.velocity = -GRAVITY;
+		if (player.velocity > GRAVITY)
+			player.velocity = GRAVITY;
+
+		if (player.velocity >= 0.f)
+			player.img.ticks_per_frame = 2;
+		else
+			player.img.ticks_per_frame = 6;
+		player.img.update();
+
+		player.y -= player.velocity;
+
+		if (player.y > SCR_H + player.img.frame_h() * 0.5f) {
+			player.y = SCR_H * 0.5f;
+			player.velocity = 0.f;
+			num_pass = 0;
+		}
+	}
+
+	void player_jump() {
+		num_pass += 1;
+		if (player.y > player.img.frame_h() * 0.5f)
+			player.velocity = 0.5f * GRAVITY;
+	}
+
+	void draw_background() {
+		game.bg.draw(game.screen);
+		game.cloud.draw(game.screen, sdl::rect(cloud_pos + SCR_W, SCR_H - game.cloud.h, game.cloud.w, game.cloud.h));
+		game.cloud.draw(game.screen, sdl::rect(cloud_pos, SCR_H - game.cloud.h, game.cloud.w, game.cloud.h));
+		game.hills.draw(game.screen, sdl::rect(hills_pos + SCR_W, SCR_H - game.hills.h, game.hills.w, game.hills.h));
+		game.hills.draw(game.screen, sdl::rect(hills_pos, SCR_H - game.hills.h, game.hills.w, game.hills.h));
+	}
+
+	void draw_player() {
+		float x = player.x - player.img.w * 0.5f;
+		float y = player.y - player.img.frame_h() * 0.5f;
+		float angle = -player.velocity * 360.f / (PI * 8.f);
+		if (angle > 85.f)
+			angle = 85.f;
+		else if (angle < -30.f)
+			angle = -30.f;
+		sdl::point p(player.img.w * 0.33f, player.img.frame_h() * 0.5f);
+		player.img.draw_angle(game.screen, sdl::rect(x, y, player.img.w, player.img.frame_h()), angle, &p);
+	}
+
 	void tick() {
+		update_background();
+		update_player();
 	}
 
 	void render() {
-		SDL_UpdateTexture(game.screen, NULL, game.pixels, 320 * sizeof(Uint32));
-		SDL_RenderCopy(game.rnd, game.screen, NULL, NULL);
-		SDL_RenderPresent(game.rnd);
+		draw_background();
+		draw_player();
+
+		game.npass.draw(game.screen, num_pass, sdl::point(SCR_W/2, 20));
+
+		game.screen.present();
 	}
 
 	void mainloop() {
@@ -95,17 +158,13 @@ namespace {
 			}
 			tick();
 			render();
-			sdl::delay_to_fps(counter);
+			sdl::delay_to_fps(counter, (Uint32)(DT * 1000.0));
 		}
 	}
 
 }
 
 int main(int argc, char* argv[]) {
-	LOG_INFO("Info test");
-	LOG_WARN("Warning test");
-	LOG_ERROR("Error test");
-	LOG_TRACE("Trace test");
 	try {
 		util::randseed();
 		sdl::sdl lib;
--- src/sdl2.hpp
+++ src/sdl2.hpp
@@ -20,10 +20,14 @@ namespace sdl {
 	};
 
 	inline Uint32 delay_to_fps(const Uint32 counter,
-	                           const Uint32 min_ticks_per_frame = (Uint32)((1.0/ 60.0) * 1000.0)) {
+	                           const Uint32 min_ticks_per_frame = (Uint32)((1.0 / 60.0) * 1000.0)) {
 		const Uint32 counter2 = SDL_GetTicks();
-		if ((counter2 > counter) && ((counter2 - counter) < min_ticks_per_frame)) {
-			SDL_Delay(min_ticks_per_frame - (counter2 - counter));
+		const Uint32 delta = counter2 - counter;
+		if (delta < min_ticks_per_frame) {//(counter2 > counter) && ((counter2 - counter) < min_ticks_per_frame)) {
+			SDL_Delay(min_ticks_per_frame - delta);
+		}
+		else {
+			LOG_TRACE("%u / %u", counter2 - counter, min_ticks_per_frame);
 		}
 		return counter2;
 	}
@@ -81,6 +85,193 @@ namespace sdl {
 		mixer _mixer;
 		image _image;
 		//ttf _ttf;
+	};
+
+	struct rect : public SDL_Rect {
+		rect() {
+			x = 0;
+			y = 0;
+			w = 0;
+			h = 0;
+		}
+		rect(const rect& o) {
+			x = o.x;
+			y = o.y;
+			w = o.w;
+			h = o.h;
+		}
+		rect(float x, float y, float w, float h) {
+			this->x = x;
+			this->y = y;
+			this->w = w;
+			this->h = h;
+		}
+		SDL_Rect* data() const {
+			return (SDL_Rect*)this;
+		}
+	};
+
+
+	struct point : public SDL_Point {
+		point() {
+			x = 0;
+			y = 0;
+		}
+		point(float x, float y) {
+			this->x = x;
+			this->y = y;
+		}
+
+		SDL_Point* data() const {
+			return (SDL_Point*)this;
+		}
+	};
+
+	struct screen {
+		SDL_Window* window;
+		SDL_Renderer* rnd;
+
+		void create(int w, int h, int winw, int winh) {
+			window = SDL_CreateWindow("simplex",
+			                          SDL_WINDOWPOS_UNDEFINED,
+			                          SDL_WINDOWPOS_UNDEFINED,
+			                          winw,
+			                          winh,
+			                          0);
+			if (window == nullptr) {
+				throw error("%s", SDL_GetError());
+			}
+			// | SDL_RENDERER_PRESENTVSYNC
+			rnd = SDL_CreateRenderer(window, -1,
+			                         SDL_RENDERER_ACCELERATED);
+			if (rnd == nullptr){
+				throw error("%s", SDL_GetError());
+			}
+			SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+			SDL_RenderSetLogicalSize(rnd, w, h);
+		}
+
+		void present() {
+			SDL_RenderPresent(rnd);
+		}
+	};
+
+	struct sprite {
+		void load(screen& s, const char* filename) {
+			SDL_Surface* img = IMG_Load(filename);
+			if (img == nullptr) {
+				throw error("Failed to load %s: %s", filename, SDL_GetError());
+			}
+			_texture = SDL_CreateTextureFromSurface(s.rnd, img);
+			if (_texture == nullptr) {
+				throw error("Failed to load %s: %s", filename, SDL_GetError());
+			}
+			if (SDL_QueryTexture(_texture, &format, &access, &w, &h) < 0) {
+				throw error("Failed to load %s: %s", filename, SDL_GetError());
+			}
+			LOG_TRACE("%s (%d, %d)", filename, w, h);
+		}
+		void draw(screen& s, const rect& src, const rect& dst) {
+			SDL_RenderCopy(s.rnd, _texture, src.data(), dst.data());
+		}
+
+		void draw(screen& s) {
+			SDL_RenderCopy(s.rnd, _texture, NULL, NULL);
+		}
+
+		void draw(screen& s, const rect& dst) {
+			SDL_RenderCopy(s.rnd, _texture, NULL, dst.data());
+		}
+
+		void draw_angle(screen& s, const rect& dst, double angle, const point* p) {
+			SDL_RenderCopyEx(s.rnd, _texture, NULL, dst.data(),
+			                 angle, (const SDL_Point*)p, SDL_FLIP_NONE);
+		}
+
+		~sprite() {
+			if (_texture) {
+				SDL_DestroyTexture(_texture);
+			}
+		}
+		SDL_Texture* _texture;
+		Uint32 format;
+		int access;
+		int w;
+		int h;
+	};
+
+	struct animated_sprite : public sprite {
+		void init_anim(int frames, int ticks_per_frame) {
+			this->frames = frames;
+			this->ticks_per_frame = ticks_per_frame;
+			dt = 0;
+			currframe = 0;
+		}
+
+		void update() {
+			dt += 1;
+			if (dt > ticks_per_frame) {
+				dt = 0;
+				currframe += 1;
+				if (currframe >= frames)
+					currframe = 0;
+			}
+		}
+
+		rect framerect() {
+			int y = frame_h() * currframe;
+			return rect(0, y, w, frame_h());
+		}
+
+		int frame_h() {
+			return h / frames;
+		}
+
+		void draw_angle(screen& s, const rect& dst, double angle, const point* p) {
+			rect f = framerect();
+			SDL_RenderCopyEx(s.rnd, _texture, f.data(), dst.data(),
+			                 angle, (const SDL_Point*)p, SDL_FLIP_NONE);
+		}
+
+		int frames;
+		int currframe;
+		int dt;
+		int ticks_per_frame;
+	};
+
+	struct numbers {
+		void load(screen& s, const char* filename) {
+			img.load(s, filename);
+		}
+
+		int _count(int number) const {
+			int np = 1;
+			while (number > 9) {
+				number /= 10;
+				np += 1;
+			}
+			return np;
+		}
+
+		void draw(screen& s, int number, const point& pos) {
+			int np = _count(number);
+			int nw = img.w / 10;
+			int tw = np * nw;
+			int x = pos.x + (tw / 2) - (nw);
+			while (true) {
+				int n = number % 10;
+
+				rect src(nw * n, 0, nw, img.h);
+				rect dst(x, pos.y - img.h / 2, nw, img.h);
+				img.draw(s, src, dst);
+				if (number < 10)
+					break;
+				number /= 10;
+				x -= nw;
+			}
+		}
+
+		sprite img;
 	};
 
 }
